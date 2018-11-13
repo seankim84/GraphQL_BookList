0cf23310e41a21e1d5aaa69d3976819bf1f51913
SeanKim <sean.kim84@gmail.com>
2018-11-13 16:42:11 +0700

    Adding the FrontEnd

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        This is Sean Graphql
+      </div>
+    );
+  }
+}
+
+export default App;
