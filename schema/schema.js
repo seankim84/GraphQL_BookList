4956088631adb75571c5fe51fe65a090c871cb00
SeanKim <sean.kim84@gmail.com>
2018-11-13 11:40:56 +0700

    Type RelationShip

--- schema/schema.js
+++ schema/schema.js
@@ -8,15 +8,15 @@ import { GraphQLObjectType,
 import _ from 'lodash';
 //dummy data
 let books = [
-    { name: "Empire", genre: "Fantasy", id: "1" },
-    { name: "The final Empire", genre:"Romance", id:"2" },
-    { name: "Egypt", genre: "Action", id:"3" }
+    { name: "Empire", genre: "Fantasy", id: "1", authorId: '1' },
+    { name: "The final Empire", genre:"Romance", id:"2", authorId: '2' },
+    { name: "Egypt", genre: "Action", id:"3", authorId: '3' }
 ];
 
 let authors = [
-    {name: "Sean.Kim", age:44, id:'1'},
-    {name: "Rebekah", age:40, id:'2'},
-    {name: "Patrick", age: 45, id:'3'}
+    { name: "Sean.Kim", age:44, id:'1' },
+    { name: "Rebekah", age:40, id:'2' },
+    { name: "Patrick", age: 45, id:'3' }
 ];
 
 
@@ -26,7 +26,14 @@ const BookType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent, args){
+               console.log(parent);
+               return _.find(authors, { id: parent.authorId })
+            }
+        }
     })
 })
 
