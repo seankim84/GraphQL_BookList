44db68f159f517ed6bdd261362d298d38d82def7
SeanKim <sean.kim84@gmail.com>
2018-11-13 13:10:15 +0700

    Mutation

--- schema/schema.js
+++ schema/schema.js
@@ -51,30 +51,53 @@ const RootQuery = new GraphQLObjectType({ // 여기서 앞서 지정한 Types의
             type: BookType,
             args: { id: { type: GraphQLID }}, // BookType을 가져와서, id라는 arg로 data를 찾는다
             resolve(parent, args){
-                console.log(typeof(args.id))
-                return  _.find(books, { id: args.id });
+                /*console.log(typeof(args.id))
+                return  _.find(books, { id: args.id });*/
             }
         },
         author: {
             type: AuthorType,
             args: { id: { type: GraphQLID }},
             resolve(parent, args){
-                return _.find(authors, { id: args.id })
+                /* return _.find(authors, { id: args.id }) */
             }
         },
         authors : {
             type: new GraphQLList(AuthorType), // 다른 type 을 정의해둔것을 갖다 사용할때는 GrahpQLList를 사용한다.
             resolve(parent, args){
-                return authors
+                /* return authors */
             }
         }
     }
 });
 
+/* Mutation */
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields:{
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name : { type: GraphQLString },
+                age : { type: GraphQLInt }
+
+            },
+            resolve(parent, args){
+                let author = new Author({
+                    name : args.name,
+                    age : args.age
+                });
+                return author.save()
+            }
+        }
+    }
+})
+
 /* export the Modules */
 
 const schema = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 });
 
 export default schema;
