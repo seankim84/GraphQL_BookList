e519aad3dbe63486ab8e610f371f67d7c0be8855
SeanKim <sean.kim84@gmail.com>
2018-11-14 14:28:29 +0700

    Refetch Query

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { graphql, compose } from 'react-apollo';
-import { getAuthorQuery, addBookMutation } from '../queries/queries';
+import { getAuthorQuery, addBookMutation, getBookQuery } from '../queries/queries';
  
 class AddBook extends Component {
 
@@ -34,7 +34,8 @@ class AddBook extends Component {
                 name: this.state.name,
                 genre: this.state.genre,
                 authorId: this.state.authorId
-            }
+            },
+            refetchQueries: [{ query: getBookQuery }]
         });
     }
 
